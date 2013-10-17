2a522ca8bb645fb0fbd072f6d164b767d61c377f
Apprentice <apprentice@dbc-20.local>
2013-10-17 14:22:59 -0700

    generates rails 3.2 skeleton without unit-test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitfolio::Application.config.secret_key_base = '4b93843e5cf35101358c2282b9e0c48a290e30b027a314f1b72bfdb4e88a6e9a278b3fba9e0aa56ae4022cb6a17b63bbaf7c71b53b5378bd7c4c843c779be70c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitfolio::Application.config.session_store :cookie_store, key: '_gitfolio_session'
