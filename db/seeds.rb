57f8bed9fd2b881857f1d5758af40fcb7c1f82f2
Apprentice <apprentice@dbc-20.local>
2013-10-17 15:44:33 -0700

    seed file loads 100 developers with up to 10 projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
+100.times do
+  dev = Developer.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    github_username: Faker::Internet.user_name
+    )
+
+  rand(10).times do
+    dev.projects.create(
+      title: Faker::Company.name,
+      url: Faker::Internet.domain_name,
+      description: Faker::Lorem.paragraph
+      )
+  end
+
+end
