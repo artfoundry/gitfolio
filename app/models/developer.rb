9340e330048103361db92990f4feb2fec832276d
Apprentice <apprentice@dbc-20.local>
2013-10-18 14:23:12 -0700

    developer model now validates on uniqueness of github username. Given that we are using GitHub Omniauth for verification, this should never actually happen -- but having a db-level validation will protect the db from being polluted in case of an uncaught error

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,6 @@
 class Developer < ActiveRecord::Base
   validates_presence_of :github_username
+  validates_uniqueness_of :github_username
   has_many :projects
   has_many :authorizations
 
--- spec/models/developer_model_spec.rb
+++ spec/models/developer_model_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe Developer do
   it { should have_many(:projects) }
   it { should validate_presence_of(:github_username) }
+  it { should validate_uniqueness_of(:github_username)}
+
+
 end
+
