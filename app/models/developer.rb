80444490cb81891984e6a80e9f92e0a0d03d7213
Apprentice <apprentice@dbc-20.local>
2013-10-18 14:34:11 -0700

    developer model now can access github data on repos and profile info

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,7 @@
+require "modules/githubdataloader.rb"
+
 class Developer < ActiveRecord::Base
+  include GitHubDataLoader
   validates_presence_of :github_username
   validates_uniqueness_of :github_username
   has_many :projects
@@ -6,5 +9,13 @@ class Developer < ActiveRecord::Base
 
   def to_param
     github_username
+  end
+
+  def all_repositories
+    return_all_repos(github_username)
+  end
+
+  def github_user_info
+    return_user_info(github_username)
   end
 end
--- spec/models/developer_model_spec.rb
+++ spec/models/developer_model_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe Developer do
+  let(:developer) {FactoryGirl.create(:developer)}
   it { should have_many(:projects) }
   it { should validate_presence_of(:github_username) }
   it { should validate_uniqueness_of(:github_username)}
 
+  it "should return an array of repos of a given developer" do
+    expect(developer.all_repositories).to be_a(Array)
+  end
+
+  it "should return user_info for a given developer" do
+    expect(developer.github_user_info).to be_a(Hash)
+    expect(developer.github_user_info.keys).to include(:github_url)
+  end
 
 end
 
