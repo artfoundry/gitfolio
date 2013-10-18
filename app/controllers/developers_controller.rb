d0cf828ccb5901321922ad76ba457c53a68e435b
Apprentice <apprentice@dbc-20.local>
2013-10-18 15:03:09 -0700

    developers controller now has repositories route, which returns a given developer's repositories scraped from github. intentional impersistence on db side

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -3,4 +3,10 @@ class DevelopersController < ApplicationController
     @developer = Developer.find_by github_username: params[:id]
     @projects = @developer.projects
   end
+
+  def repositories
+    developer = Developer.find_by github_username: params[:id]
+    @repositories =developer.return_all_repos(params[:id])
+    render nothing: true
+  end
 end
--- spec/controllers/developers_controller_spec.rb
+++ spec/controllers/developers_controller_spec.rb
@@ -9,4 +9,20 @@ describe DevelopersController do
       expect(assigns(:developer)).to be_a(Developer)
     end
   end
+
+  describe "POST developer#repositories" do
+    before(:each) do
+      @developer = Developer.create(github_username: 'nscricco')
+    end
+
+    after(:each) do
+      @developer.destroy
+    end
+
+    it "should show a developer's repositories" do
+      post :repositories, {id: @developer.github_username}
+      expect(assigns(:repositories).map {|repo| repo[:name]}).to include('quote_racer')
+    end
+
+  end
 end
