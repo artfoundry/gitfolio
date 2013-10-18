d8902a1b3122827e612ff88b56fc72b6aa794f11
Douglas Mill <dmill989@gmail.com>
2013-10-18 14:03:55 -0700

    adds sessions controller test for destroy action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,10 +4,16 @@ describe SessionsController do
   before do
     request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
   end
-  describe "POST sessions#create" do
+  describe "GET sessions#create" do
     it "should assign a developer_id to session" do
     	get :create, provider: 'github'
     	expect(session[:developer_id]).to be_a(Integer)
+    end
+  end
+    describe "GET sessions#destroy" do
+      it "should remove a developer_id from the session" do
+      get :destroy, provider: 'github'
+      expect(session[:developer_id]).to be_nil
     end
   end
 end
