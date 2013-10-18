d9258b7df2c216867c4ece3ba5bcbb09430fca82
Douglas Mill <dmill989@gmail.com>
2013-10-18 15:15:40 -0700

    remove app id and secret from initializers file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-APP_ID = '2f457624a6b5fbae8c03'
-APP_SECRET = '8efff932356f70adbc75a1be60152fc591b5ad29'
+# APP_ID = '2f457624a6b5fbae8c03'
+# APP_SECRET = '8efff932356f70adbc75a1be60152fc591b5ad29'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, APP_ID, APP_SECRET
