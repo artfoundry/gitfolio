67ea7c61b691703527e7ef13733da269f7199714
Douglas Mill <dmill989@gmail.com>
2013-10-18 15:21:05 -0700

    removes app id and secret comment from omniauth file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
-# APP_ID = '2f457624a6b5fbae8c03'
-# APP_SECRET = '8efff932356f70adbc75a1be60152fc591b5ad29'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['APP_ID'], ENV['APP_SECRET']
 end
