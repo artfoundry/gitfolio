525626583f36db6cac7a6748ad1addaf4e978f3a
Douglas Mill <dmill989@gmail.com>
2013-10-19 17:20:34 -0700

    adds logic for rendirecting to edit developer route only for a new user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,15 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
+    authorization_count = Authorization.count
     auth = Authorization.find_or_create(auth_hash)
     developer = auth.developer
     session[:developer_id] = developer.id
-
-    redirect_to developer_path(developer)
+    if authorization_count != Authorization.count
+      redirect_to edit_developer_path(developer)
+    else
+      redirect_to developer_path(developer)
+    end
   end
 
   def destroy
