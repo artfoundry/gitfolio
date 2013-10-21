0f6e05668ec9a3f5e3a3df657e696f8236a4c0ca
srinivas rao <savinirs@gmail.com>
2013-10-21 11:50:23 -0700

    user clicking a repo button now returns an array of hash objects
    - each hash refers to a single file in that repo.
    - functionality to return content of file written, but not integrated
    - appending back to dom is non functional right now
    - removing elements kills all forms, not just the current one.

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,8 +1,63 @@
 
 $('.file-loader').on('ajax:success',loadfiles)
 
-function loadfiles() {
-  var $container = $( '.github-container' )
-  $container.remove()
-  console.log('started from the bottom now we here')
+function loadfiles(event, repoData) {
+  removeRows()
+  console.log('started from the bottom')
+  loadFiles(repoData)
+
+
+}
+
+function removeRows() {
+  var $repoRows = $( '.github-container' )
+  $repoRows.remove()
+}
+
+function loadFiles(repoData) {
+  var owner = repoData.owner
+  var repo = repoData.repo_name
+  var files = repoData.files
+
+  var files_hash = createFileHashes(owner, repo, files)
+  createFileElements(files_hash)
+
+}
+
+function createFileElements(files) {
+  $.each(files, function(i,value) {
+    element = $('<div/>').text("some text")
+    console.log(element.text())
+    element.appendTo('#secondary-container')
+  })
+}
+
+
+function createFileHashes(owner, repo, files) {
+
+  var files_hash = []
+
+  $.each(files, function(key,value) {
+    file_hash = {}
+    file_hash["owner"] = owner
+    file_hash["repo"] = repo
+    file_hash["path"] = value
+    file_hash["name"] = key
+    files_hash.push(file_hash)
+    })
+
+  return files_hash
+}
+
+function getFileCode() {
+  $.ajax({
+    url: '/get_file_info',
+    type: 'POST',
+    data: { owner: "raorao", path: "Gemfile", repo: "ar-student-schema" },
+    complete: raorao
+  })
+}
+
+function raorao() {
+ console.log("now my whole team fucking here")
 }
