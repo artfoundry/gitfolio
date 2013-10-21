409e7679a7616431d64589840a22a04133bdd5f0
srinivas rao <savinirs@gmail.com>
2013-10-21 15:43:59 -0700

    SLI basic set up
    projects have many chunks, and chunks can either be images, codesnippets, or describers (e.g. all text). query interface supports all model names.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :developer
   has_many :chunks
+  has_many :images
+  has_many :describers
+  has_many :code_snippets
 end
--- spec/models/project_model_spec.rb
+++ spec/models/project_model_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe Project do
   it { should belong_to(:developer) }
   it { should have_many(:chunks) }
+  it { should have_many(:chunks) }
+  it { should have_many(:code_snippets) }
+  it { should have_many(:describers) }
+
 end
