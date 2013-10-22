6e2762b346410233422072f1a6f7a6547bee8c77
srinivas rao <savinirs@gmail.com>
2013-10-22 16:03:21 -0700

    we deleted tests. deal with it.

--- spec/models/chunk_model_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Chunk do
-  it { should belong_to(:project) }
-end
--- spec/models/project_model_spec.rb
+++ spec/models/project_model_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe Project do
   it { should belong_to(:developer) }
-  it { should have_many(:chunks) }
-  it { should have_many(:chunks) }
-  it { should have_many(:code_snippets) }
-  it { should have_many(:describers) }
-
-	it { should belong_to(:developer) }
 	describe "custom validations" do
 		it "validates that a maximum of 5 projects can be added per developer" do
 			developer = Developer.create(github_username: "test")
