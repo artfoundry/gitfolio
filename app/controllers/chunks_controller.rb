1d04420ca66708c24b2a03b87dbce90f99046e1a
srinivas rao <savinirs@gmail.com>
2013-10-21 21:48:38 -0700

    form now implements project id

--- app/controllers/chunks_controller.rb
+++ app/controllers/chunks_controller.rb
@@ -3,18 +3,18 @@ class ChunksController < ApplicationController
   def create
     new_chunk = Chunk.create(valid_params(params))
 
-    render :text => "this is working"
+    render :text => params
   end
 
   private
 
   def valid_params(params)
 
-    return params[:describer].permit(:title, :url, :textfield) if params[:describer]
+    return params[:describer].permit(:title, :url, :textfield, :project_id) if params[:describer]
 
-    return params[:image].permit(:title, :url, :description) if params[:image]
+    return params[:image].permit(:title, :url, :description, :project_id) if params[:image]
 
-    return params[:code_snippet].permit(:title, :description, :code) if params[:code_snippet]
+    return params[:code_snippet].permit(:title, :description, :code, :project_id) if params[:code_snippet]
 
 
   end
