d5cf6f54607f77aa43587ac5edcf2c766c504473
srinivas rao <savinirs@gmail.com>
2013-10-22 17:26:51 -0700

    changing method names to avoice namespace collisions

--- lib/modules/markdowner.rb
+++ lib/modules/markdowner.rb
@@ -4,7 +4,7 @@ module MarkDowner
 
   extend self
 
-  def convert(html_text)
+  def markdown_convert(html_text)
     BlueCloth.new(html_text).to_html
   end
 
@@ -19,6 +19,6 @@ A Second Level Header
 
 expected_output = "<h1>A First Level Header</h1>\n\n<h2>A Second Level Header</h2>"
 
-p MarkDowner.convert(test_input) == expected_output
+p MarkDowner.markdown_convert(test_input) == expected_output
 
 
