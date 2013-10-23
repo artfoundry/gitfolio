5e4f075d6d387435b167fa213c7da6dae18953af
Douglas Mill <dmill989@gmail.com>
2013-10-23 11:35:26 -0700

    adds project instance method for returning number of forks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
+require "modules/githubdataloader.rb"
+
 class Project < ActiveRecord::Base
+  include GitHubDataLoader
   include ActiveModel::Validations
   belongs_to :developer
   has_many :chunks
@@ -6,5 +9,9 @@ class Project < ActiveRecord::Base
   has_many :describers
   has_many :code_snippets
   validates_with ProjectMaximumValidator
+
+  def get_forks
+	return_number_of_forks(self.developer.github_username, self.title)
+  end
 
 end
