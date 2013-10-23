20a656d3fad1342319ca7c0ed059c356adfcd73c
Douglas Mill <dmill989@gmail.com>
2013-10-23 11:41:09 -0700

    removes old chunk bs from project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,10 +4,6 @@ class Project < ActiveRecord::Base
   include GitHubDataLoader
   include ActiveModel::Validations
   belongs_to :developer
-  has_many :chunks
-  has_many :images
-  has_many :describers
-  has_many :code_snippets
   validates_with ProjectMaximumValidator
 
   def get_forks
