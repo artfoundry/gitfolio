6bbdd1edaf91983448c7e026583f151d3ebb22ca
srinivas rao <savinirs@gmail.com>
2013-10-22 21:54:24 -0700

    removed stealth integration test

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -13,21 +13,6 @@ describe ProjectsController do
     end
   end
 
-  describe "POST projects#create_from_repos" do
-    let(:developer) { FactoryGirl.create(:developer) }
-
-    it "should create a project from a repository" do
-      expect{
-        post :create_from_repo, {
-          owner: developer.github_username,
-          name: "some repo",
-          url: "someurl",
-          description: "some description"}
-      }.to change {Project.count}.by(1)
-    end
-
-  end
-
   describe "PATCH projects#update" do
     let(:project) { FactoryGirl.create(:project) }
     let(:new_project) { FactoryGirl.build(:project) }
