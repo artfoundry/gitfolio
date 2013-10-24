9539a86931ee64e3deaa01d7bef2c1909d12340f
Apprentice <apprentice@dbc-21.local>
2013-10-23 20:30:45 -0700

    fixes bug disallowing edits when there are five projects

--- app/validators/project_maximum_validator.rb
+++ app/validators/project_maximum_validator.rb
@@ -1,6 +1,6 @@
 class ProjectMaximumValidator < ActiveModel::Validator
 	def validate(record)
-		unless record.developer.projects.count < 5
+		unless record.developer.projects.count <= 5
 			record.errors[:project_max] << "You have reached the maximum number of projects allowed"
 		end
 	end
